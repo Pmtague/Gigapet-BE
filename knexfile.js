c61220a5a7b44a0c197b2b13d28578e7573f43a3
Neha Patel <neha.r.p@gmail.com>
2019-09-23 13:28:32 -0700

    created knexfile.js
    Co-authored-by: Penny Tague <pmtague9@gmail.com>

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: { filename: './database/gigapet.db3' },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: { filename: './database/test.db3' },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  },
+};
