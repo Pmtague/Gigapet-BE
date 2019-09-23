dc19891bdc8efcc3d5802343a8ccc1b42d11f381
Neha Patel <neha.r.p@gmail.com>
2019-09-23 13:25:46 -0700

    server.get working
    Co-authored-by: Penny Tague <pmtague9@gmail.com>

--- api/server.js
+++ api/server.js
@@ -8,4 +8,8 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "running" });
+});
+
 module.exports = server;
