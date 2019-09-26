b974bd49f3a0162facc9f018e3b4714ac879a23e
Neha Patel <neha.r.p@gmail.com>
2019-09-26 14:49:42 -0700

    Revert "remove() test for to users-model.spec.js"
    This reverts commit de0793f588484fb8d0507346a91e8aeb7a32a8a0.

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -2,7 +2,6 @@ const Users = require("./users-model.js");
 const db = require("../database/db-config.js");
 
 describe("users-model.js", () => {
- 
   beforeEach(async () => {
     await db("users").truncate();
   });
@@ -24,29 +23,16 @@ describe("users-model.js", () => {
     });
 
     it("should insert users into the db", async () => {
-      const { id } = await Users.add({
-        username: "penny",
-        password: "tague"
-      });
+        const { id } = await Users.add({
+            username:"penny",
+            password: "tague"
+        })
 
-      let user = await db("users")
-        .where({ id })
-        .first();
+        let user = await db("users")
+            .where({ id })
+            .first()
 
-      expect(user.username).toBe("penny");
-    });
-  });
-  describe("remove()", () => {
-    it("should remove user from the db", async () => {
-
-      const { id } = await Users.remove({
-        username: "Wayne",
-        password: "brett"
-      });
-
-      let users = await db("users");
-      
-      expect(users).toHaveLength(0);
+        expect(user.username).toBe("penny")
     });
   });
 });
